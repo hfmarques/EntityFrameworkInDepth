4d9d316f897e0087ebe093160b4fcc75388185df
Heber Marques <heber.marques@umusic.com>
2022-08-17 09:08:56 -0300

    Added project DataAnnotations

---
+++ DataAnnotations/PlutoContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAnnotations
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server={server};Initial Catalog={database};Persist Security Info=False;User ID={user};Password={password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ DataAnnotations/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using DataAnnotations;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var connectionString = configuration.GetConnectionString("Pluto");
+
+Console.WriteLine(connectionString);
+
+using var context = new PlutoContext();
+
+var authors = context.Authors.ToList();
+
+Console.WriteLine(JsonSerializer.Serialize(authors));
+
+Console.ReadLine();
